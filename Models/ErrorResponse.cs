7f92dee927593e6f8c43a9f3b7aee559dca3952e
adeojoemmanueli <emmanuel@xroads.tech>
2024-12-11 00:20:17 +0100

    feat: see sharp todo app

---
+++ Models/ErrorResponse.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class ErrorResponse
+    {
+        public string Title { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
